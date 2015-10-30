7969927c02c806e740adb0b03b6ccb1b52fbd851
GianhTran <mrgianh92@gmail.com>
2015-10-30 10:29:48 +0700

    add missing file

---
+++ app/src/main/java/vn/asiantech/intership/myapplication/dialog/CoachInFreeZone/LoadListCoachInFreeZoneInterface.java
@@ -0,0 +1,12 @@
+package vn.asiantech.intership.myapplication.dialog.CoachInFreeZone;
+
+import java.util.List;
+
+import vn.asiantech.intership.myapplication.model.Coach;
+
+/**
+ * Created by igianhtran on 28/10/2015.
+ */
+public interface LoadListCoachInFreeZoneInterface {
+    void loadSuccess(List<Coach> coaches);
+}
---
+++ app/src/main/java/vn/asiantech/intership/myapplication/ui/league/GetListNumberofTeamInterface.java
@@ -0,0 +1,10 @@
+package vn.asiantech.intership.myapplication.ui.league;
+
+import java.util.List;
+
+/**
+ * Created by igianhtran on 30/10/2015.
+ */
+public interface GetListNumberofTeamInterface {
+    void getSuccess(List<String> list);
+}
